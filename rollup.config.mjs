9902be9a82205858685fa1dbd4a8a4f43812a443
Baha Alimi <77264101+3llimi@users.noreply.github.com>
2024-06-30 21:41:13 +0300

    Add files via upload

---
+++ rollup.config.mjs
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'cjs',
+        sourcemap: true,
+    },
+    plugins: [
+        typescript(),
+    ],
+    external: ['date-fns'],
+};
