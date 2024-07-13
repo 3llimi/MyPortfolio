443e28437324d977d87a50b4d7e39a5896456bdf
Baha Alimi <a.alimi@innopolis.university>
2024-07-13 23:36:30 +0300

    short fix

--- src/lib/comicData.ts
+++ src/lib/comicData.ts
@@ -1,9 +1,9 @@
 export interface ComicData {
-    id: string;
-    safe_title: string;
-    img: string;
-    alt: string;
-    year: number;
-    month: number;
-    day: number;
+  id: string
+  safe_title: string
+  img: string
+  alt: string
+  year: number
+  month: number
+  day: number
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,96 +1,94 @@
-  <head>
-    <title>3llimi's Portfolio</title>
-    <link rel="icon" type="image/png" href="/src/assets/2229_rammus_okay.png">
-    <meta name="author" content="Ahmed Baha Eddine Alimi">
-    <meta name="description" content="This My Portfolio">
-  
-    <link rel="stylesheet" href="/src/global.css">
+<head>
+  <title>Allimi's Portfolio</title>
+  <link rel="icon" type="image/png" href="/src/assets/2229_rammus_okay.png" />
+  <meta name="author" content="Ahmed Baha Eddine Alimi" />
+  <meta name="description" content="This My Portfolio" />
 
-  
-    <slot name="head"></slot>
-  </head>
-  
-  <body>
-    <nav>
-      <div class="nav">
-        <h1 class="titles">
-          <a href="/about">HOME</a>
-        </h1>
-        <h1 class="titles">
-          <a href="/comic">COMIC</a>
-        </h1>
-      </div>
-    </nav>
-  
-    <slot></slot>
-  
-    <footer class="P5">
+  <link rel="stylesheet" href="/src/global.css" />
+
+  <slot name="head"></slot>
+</head>
+
+<body>
+  <nav>
+    <div class="nav">
+      <h1 class="titles">
+        <a href="/about">HOME</a>
+      </h1>
+      <h1 class="titles">
+        <a href="/comic">COMIC</a>
+      </h1>
+    </div>
+  </nav>
+
+  <slot></slot>
+
+  <footer class="P5">
+    <div>
       <div>
-        <div>
-          <h1 class="getintouch">GET IN TOUCH</h1>
-          <a id="email" href="mailto:3llimi69@gmail.com">
-            <h1 class="email">3llimi69@gmail.com</h1>
-          </a>
-          <h6 class="copyrights">
-            © 2024 Designed and Developed by Ahmed Baha Eddine Alimi
-          </h6>
-        </div>
+        <h1 class="getintouch">GET IN TOUCH</h1>
+        <a id="email" href="mailto:3llimi69@gmail.com">
+          <h1 class="email">3llimi69@gmail.com</h1>
+        </a>
+        <h6 class="copyrights">
+          © 2024 Designed and Developed by Ahmed Baha Eddine Alimi
+        </h6>
       </div>
-    </footer>
-  </body>
-  
-  
-  <style>
-    body {
-    font-family: "Satoshi", sans-serif;
+    </div>
+  </footer>
+</body>
+
+<style>
+  body {
+    font-family: 'Satoshi', sans-serif;
     margin: 0;
     padding: 0;
-}
+  }
 
-.nav {
+  .nav {
     display: flex;
     flex-direction: row;
     column-gap: 20px;
-    font-family: "Satoshi", sans-serif;
+    font-family: 'Satoshi', sans-serif;
     font-weight: 900;
     justify-content: center;
     background-color: black;
-}
+  }
 
-.titles {
+  .titles {
     color: white;
     transition: color 0.5s ease-in-out;
-}
+  }
 
-.titles:hover {
+  .titles:hover {
     color: orange;
-}
+  }
 
-a {
+  a {
     text-decoration: none;
     color: inherit;
-}
+  }
 
-.getintouch {
-    font-family: "Satoshi", sans-serif;
+  .getintouch {
+    font-family: 'Satoshi', sans-serif;
     font-weight: 1000;
-}
+  }
 
-.copyrights {
-    font-family: "Satoshi", sans-serif;
+  .copyrights {
+    font-family: 'Satoshi', sans-serif;
     font-weight: 400;
     font-style: italic;
-}
+  }
 
-.email {
-    font-family: "Satoshi", sans-serif;
+  .email {
+    font-family: 'Satoshi', sans-serif;
     font-weight: 900;
     margin: 30px;
     color: black;
     font-size: 1em;
-}
+  }
 
-.P5 {
+  .P5 {
     background-color: #e7e7e7;
     height: 15vh;
     display: flex;
@@ -100,5 +98,5 @@ a {
     flex-direction: column;
     text-align: center;
     line-height: 1px;
-}
-  </style>
+  }
+</style>
