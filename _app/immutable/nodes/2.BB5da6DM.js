184bbf896779f8409a710f848e85e9eada0aa1ea
Baha Alimi <a.alimi@innopolis.university>
2024-07-14 23:37:23 +0300

    Updates

---
+++ _app/immutable/nodes/2.BB5da6DM.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.BsZpeh6m.js";import{S as t,i as e}from"../chunks/index.Bu-wHBV3.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
